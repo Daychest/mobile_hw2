985a3287d6273708b7b0d5430994ef79713fd148
Daychest <veikka.paivarinta@gmail.com>
2026-01-29 22:04:44 +0200

    fixed error

--- app/src/main/java/com/example/mobile_hw2/MainActivity.kt
+++ app/src/main/java/com/example/mobile_hw2/MainActivity.kt
@@ -13,28 +13,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposableOpenTarget
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
-import androidx.navigation.NavHost
-import androidx.navigation.NavHostController
-import androidx.navigation.Navigation
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.mobile_hw2.ui.theme.Mobile_hw2Theme
 import kotlinx.serialization.Serializable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
@@ -55,6 +48,7 @@ object Profile
 @Serializable
 object FriendsList
 
+@Preview
 @Composable
 fun Navigation(){
     val navController = rememberNavController()
@@ -100,7 +94,7 @@ fun MainScreen(navController: NavController){
 }
 
 @Composable
-fun DetailScreen(name: String){
+fun DetailScreen(name: String?){
     Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()){
         Text(text = "Hello $name")
     }
