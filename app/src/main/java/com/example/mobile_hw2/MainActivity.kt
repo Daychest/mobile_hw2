2fe068753547eef3f21565d1b54bf1aceb736609
Daychest <veikka.paivarinta@gmail.com>
2026-01-30 20:14:13 +0200

    added conversation code

--- app/src/main/java/com/example/mobile_hw2/MainActivity.kt
+++ app/src/main/java/com/example/mobile_hw2/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.mobile_hw2
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
@@ -26,10 +43,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
+import com.example.mobile_hw2.ui.theme.Mobile_hw2Theme
 
 
 class MainActivity : ComponentActivity() {
@@ -51,25 +72,15 @@ fun Navigation() {
             MainScreen(navController = navController)
         }
         composable(
-            route = Screen.DetailScreen.route + "/{name}",
-            arguments = listOf(
-                navArgument("name") {
-                    type = NavType.StringType
-                    defaultValue = "Philipp"
-                    nullable = true
-                }
-            )
+            route = Screen.DetailScreen.route,
         ) { entry ->
-            DetailScreen(name = entry.arguments?.getString("name"), navController = navController)
+            DetailScreen(navController = navController)
         }
     }
 }
 
 @Composable
 fun MainScreen(navController: NavController) {
-    var text by remember {
-        mutableStateOf("")
-    }
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -78,16 +89,10 @@ fun MainScreen(navController: NavController) {
             .fillMaxSize()
             .padding(horizontal = 50.dp)
     ) {
-        TextField(
-            value = text, onValueChange = {
-                text = it
-            },
-            modifier = Modifier.fillMaxWidth()
-        )
         Spacer(modifier = Modifier.height(8.dp))
         Button(
             onClick = {
-                navController.navigate(Screen.DetailScreen.withArgs(text))
+                navController.navigate(Screen.DetailScreen.route)
             },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
@@ -97,10 +102,8 @@ fun MainScreen(navController: NavController) {
 }
 
 @Composable
-fun DetailScreen(navController: NavController, name: String?) {
-    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-        Text(text = "Hello $name")
-    }
+fun DetailScreen(navController: NavController) {
+    //Column() { }
     Button(
         onClick = {
             navController.navigate(Screen.MainScreen.route) {
@@ -110,7 +113,80 @@ fun DetailScreen(navController: NavController, name: String?) {
             }
         },
     ) {
-        Text(text = "To Mainscreen")
+        Text(text = "Back")
     }
 }
 
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.reaction),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    Mobile_hw2Theme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+
+data class Message(val author: String, val body: String)
